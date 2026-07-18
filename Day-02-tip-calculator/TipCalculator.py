ab6deb0b6f4d834792230d194624455629e59900
Sambhav-12 <sambhav12dev@gmail.com>
2026-07-18 11:14:19 +0530

    Delete Day-02-tip-calculator directory

--- Day-02-tip-calculator/TipCalculator.py
+++
@@ -1,8 +0,0 @@
-print("Welcome to the tip calculator!")
-bill = float(input("What was the total bill - $"))
-tip = int(input("How much tip would you like to give? 10, 12 or 15? - "))
-total_bill = bill * (tip/100)
-s_people = int(input("How many people to split the bill - "))
-individual_bill = round(((total_bill + bill)/s_people), 2)
-print(f"Each person should pay: ${individual_bill}")
-
