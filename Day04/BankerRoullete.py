176d4e3607be00178c37465c5ddd0523b0f41fe1
Sambhav <sambhav12dev@gmail.com>
2026-07-18 19:05:34 +0530

    Delete Day04 directory

--- Day04/BankerRoullete.py
+++
@@ -1,8 +0,0 @@
-import random
-friends = ["Soul" , "Gk" , "Ace" , "NicePerson" , "Tama" , "Ryan"]
-n = random.randint(0,5)
-print(friends[n])
-
-# Other way
-c = random.choice(friends)
-print(c)
--- Day04/Head&Tails.py
+++
@@ -1,9 +0,0 @@
-import random
-
-# 0 = Heads
-# 1 = Tails
-n = random.randint(0,1)
-if n == 0:
-    print("Heads")
-else:
-    print("Tails")
