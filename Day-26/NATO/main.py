7be65505cc1db26a96fe1ea806b97eff631177cb
Sam <sambhav12dev@gmail.com>
2026-08-08 22:20:54 +0530

    Add files via upload

---
+++ Day-26/NATO/main.py
@@ -0,0 +1,9 @@
+import pandas
+
+data = pandas.read_csv("DAY-26/NATO/nato_phonetic_alphabet.csv")
+nato_dict = {row.letter:row.code for (index, row) in data.iterrows()}
+
+words = input("Enter the word: ").replace(" ", "").upper()
+print(words)
+phonetic_list = [nato_dict[word] for word in words ]
+print(phonetic_list)
